dd8512345c77b8d8499ec478ece075ead60d8a38
Clayton William <clayton.calvin@gmail.com>
2018-09-22 18:40:43 -0400

    update subtraction; mulitplication and division to the example

--- basics/clean_url.py
+++ basics/clean_url.py
@@ -1,6 +1,8 @@
 """learning the basics of flask"""
 
+from __future__ import division
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -20,6 +22,33 @@ def index(name="world"):
 def add(num1, num2):
     return '{} + {} = {}'.format(num1, num2, num1+num2)
 
+
+@app.route('/subtract/<int:num1>/<int:num2>')
+@app.route('/subtract/<float:num1>/<float:num2>')
+@app.route('/subtract/<float:num1>/<int:num2>')
+@app.route('/subtract/<int:num1>/<float:num2>')
+def subtract(num1, num2):
+    return '{} - {} = {}'.format(num1, num2, num1-num2)
+
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+@app.route('/multiply/<float:num1>/<float:num2>')
+@app.route('/multiply/<float:num1>/<int:num2>')
+@app.route('/multiply/<int:num1>/<float:num2>')
+def multiply(num1, num2):
+    return '{} * {} = {}'.format(num1, num2, num1*num2)
+
+
+@app.route('/divide/<int:num1>/<int:num2>')
+@app.route('/divide/<float:num1>/<float:num2>')
+@app.route('/divide/<float:num1>/<int:num2>')
+@app.route('/divide/<int:num1>/<float:num2>')
+def divide(num1, num2):
+    try:
+        solution = num1 / num2
+    except ZeroDivisionError as why:
+        return '{} / {} = {}'.format(num1, num2, why) + ' is illegal.'
+    return '{} / {} = {}'.format(num1, num2, solution)
 
 app.run(debug=True)
 
